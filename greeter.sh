48a4417882a09261d375bfff1e06c6e060a352a4
David Lawson <39196767+linuxlawson@users.noreply.github.com>
2022-04-26 19:11:12 -0400

    added to

--- greeter.sh
+++ greeter.sh
@@ -14,7 +14,7 @@ clear
 echo
 
 sleep 1
-echo -e "${YELLOW}Greetings $name${NOCOLOR}\n"
+echo -e "${YELLOW}Greetings $name ${NOCOLOR}\n"
 
 sleep 1
 echo -e "${YELLOW}How are you feeling today?${NOCOLOR}\n"
@@ -29,25 +29,35 @@ echo -e "\n${YELLOW}How about a game of chess?${NOCOLOR}\n"
 
 read -r
 
-
 sleep 1
 echo -en "\n${YELLOW}Fine.${NOCOLOR}\n"
-sleep 1
 
 
 #pin/password required
-echo -e "\n${YELLOW}PIN required before proceeding${NOCOLOR}\n"
 sleep 1
+echo -e "\n${YELLOW}PIN required before proceeding${NOCOLOR}\n"
 
-
+sleep 1
 read -rp "$(echo -e $YELLOW"PIN: "$NOCOLOR)" 
 
+sleep 1
+echo -e "\n${YELLOW}Authenticating..${NOCOLOR}"
 
 sleep 1
-#clear
-echo -e "\n\n${YELLOW}Access Granted.${NOCOLOR}\n\n"
+echo -e "${YELLOW}Process Complete${NOCOLOR}"
 
+
+#combined read commands 
+#seperated by semicolons
 sleep 1
+echo -en ${YELLOW}
+(read -p "Access " -t .5); 
+(read -p "Granted" -t .5); 
+(read -p " " -t 1)
+echo -en ${YELLOW}"\n"
+
+
+#clear screen and message
 clear
 echo -e ${YELLOW}
 echo -e "##########################################################"
@@ -55,5 +65,4 @@ echo -e "##   WARNING!! SECURE AREA. AUTHORIZED PERSONAL ONLY!   ##"
 echo -e "##   All activities are being monitored and recorded.   ##"
 echo -e "##########################################################\n"
 echo -e ${NOCOLOR}
-
 
